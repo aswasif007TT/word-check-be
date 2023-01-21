2f5be3863b3d7054c19af024ad0a3b72d3433920
Ahmed S. Wasif <ahmed.wasif@toptal.com>
2023-01-21 10:42:46 +0600

    Build the prefix-tree when instantiating the server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DictionaryService } from './tasks/dictionary.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, async () => {
+    console.log(`Server started.`);
+
+    // Build dictionary on startup
+    await app.get(DictionaryService).buildDictionary();
+  });
 }
 bootstrap();
