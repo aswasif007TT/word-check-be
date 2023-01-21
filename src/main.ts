7e75649f95d1fd37122b4857e1f7307e5f9a6ee2
Ahmed S. Wasif <ahmed.wasif@toptal.com>
2023-01-21 15:07:59 +0600

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { DictionaryService } from './tasks/dictionary.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Todo: maintain a whitelist of allowed origins
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000, async () => {
     console.log(`Server started.`);
 
